769b4c3f7af529ed0bc2093bc7ff2fbc91cd42ea
marty-p <marty-p@outlook.com>
2018-11-20 13:14:24 +0100

    +base

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+	Rigidbody rb;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Use this for initialization
+	void FixedUpdate () {
+		float speed = Random.Range(0.5f, 1.2f);
+		Vector3 position = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+		// transform.position += position * speed;
+		rb.AddForce(position * speed, ForceMode.Impulse);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	int health = 100;
+	Rigidbody rb;
+	public Image healthBar;
+	public Text healthText;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update() {
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float speed = 10.0f;
+        float movementH = Input.GetAxis("Horizontal") * speed;
+        float movementV = Input.GetAxis("Vertical") * speed;
+        float movementJ = 0;
+        Vector3 movement = new Vector3(movementH, movementJ, movementV);
+        rb.AddForce(movement);
+	}
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag("Enemy") && health > 0) {
+			health -= 10;
+			healthBar.fillAmount = health / 100f;
+
+			// int tileDimension = 36;
+            // Rect rect = healthBar.rectTransform.rect;
+            // healthBar.rectTransform.sizeDelta = new Vector2(rect.width - tileDimension, rect.height);
+
+			healthText.text = health.ToString() + '%';
+
+			// Debug.Log(health);
+			// Debug.Log(healthBar.fillAmount);
+		}
+	}
+}
